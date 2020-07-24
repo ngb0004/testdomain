3d06da81a856f1a2866019fe968d3cbf57b81d88
Nick Biester <nickbiester325@gmail.com>
2020-07-23 21:22:20 -0400

    test3

--- script.js
+++ script.js
@@ -1,39 +1,39 @@
 
 
-const petForm = document.querySelector("#pet-form");
+// const petForm = document.querySelector("#pet-form");
 
-petForm.addEventListener("submit", fetchAnimals);
+// petForm.addEventListener("submit", fetchAnimals);
 
-//fetch animals from API
-function fetchAnimals(e){
-    e.preventDefault();
+// //fetch animals from API
+// function fetchAnimals(e){
+//     e.preventDefault();
     
-    //get user input
-    const key = "CIbntt4JECoiWAAsvrXjBl6d0U2btsVpxB2RvYeDvgbvMOfqC9";
-    const secret = "f6UaLPI7VZCvetLSQocjTKt0HcMUktilmMOmmcE1"
-    const zip = document.querySelector("#zip").value;
-    let queryURL = `https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=${key}&client_secret=${secret}`;
+//     //get user input
+//     const key = "CIbntt4JECoiWAAsvrXjBl6d0U2btsVpxB2RvYeDvgbvMOfqC9";
+//     const secret = "f6UaLPI7VZCvetLSQocjTKt0HcMUktilmMOmmcE1"
+//     const zip = document.querySelector("#zip").value;
+//     let queryURL = `https://api.petfinder.com/v2/oauth2/token?grant_type=client_credentials&client_id=${key}&client_secret=${secret}`;
 
-    $.ajax({
-        dataType: "json",
-        url: queryURL,
-        method: "GET",     
-     })
+//     $.ajax({
+//         dataType: "json",
+//         url: queryURL,
+//         method: "GET",     
+//      })
     
-    .then(function(data) {
-        console.log(data);
+//     .then(function(data) {
+//         console.log(data);
 
 
-    })
-    .catch(function(err) {
-        console.log(err);
-    })
-}
+//     })
+//     .catch(function(err) {
+//         console.log(err);
+//     })
+// }
 
-function callback(data) {
+// function callback(data) {
 
-    console.log(data);
-}
+//     console.log(data);
+// }
 
 function refreshTokens() {
      //get user input
